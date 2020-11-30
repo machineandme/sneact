f2db1bf866a6fd3837a2cbfa282f36bf0f352876
Nikolay Kiselev <nkiselev@pop-os.localdomain>
2020-11-30 14:54:06 +0300

    Update readme

--- sneact/__init__.py
+++ sneact/__init__.py
@@ -2,7 +2,7 @@ from sneact._magic_tags import MagicHTMLTag
 from sneact._scopes import _SneactScope
 from sneact._sneact import Sneact
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 _ = MagicHTMLTag("\n")
 s = _SneactScope()
