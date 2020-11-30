df9c3c47a6da3d219724240ca298d2240274ac0c
Nikolay Kiselev <nkiselev@pop-os.localdomain>
2020-11-30 14:44:16 +0300

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+from pytest import fixture
+
+
+class FakeObject:
+    def __init__(self):
+        self.token = "x"
+        self.segments = ("x",)
+
+
+@fixture
+def fake_magic_class():
+    fake = FakeObject()
+    return fake
+
+
+@fixture
+def smoke_sneact_expected_results():
+    return (
+        "\n".join(
+            (
+                "<div>",
+                "<p>Tiger</p>",
+                "<p>About tigers</p>",
+                "<div>",
+                "<p>Hello tigers. We love Tigers.</p>",
+                '<img src="tiger.png">',
+                "</div>",
+                "</div>",
+            )
+        )
+        + "\n"
+    )
+
+
+@fixture
+def nested_expected_results():
+    return (
+        "\n".join(
+            (
+                "<header>",
+                "<h1>Page</h1>",
+                "</header>",
+                "<p>Lorem you know</p>",
+                "<footer>",
+                "<p>Copyright Kiselev Nikolay 2020</p>",
+                "</footer>",
+            )
+        )
+        + "\n"
+    )
